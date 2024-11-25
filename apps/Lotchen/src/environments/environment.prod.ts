9976e3921e9a6d2a6e1a2906209f930ea63e950a
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2024-11-25 07:01:40 +0100

    feat: update environment variablee

--- apps/Lotchen/src/environments/environment.prod.ts
+++ apps/Lotchen/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { EnvironmentConfig } from '@talisoft/common';
 
 export const environment: EnvironmentConfig = {
   production: false,
-  apiUrl: 'https://localhost:54978',
+  apiUrl: 'https://localhost:44363',
   apiKey: '',
 };
--- apps/Lotchen/src/environments/environment.ts
+++ apps/Lotchen/src/environments/environment.ts
@@ -2,6 +2,6 @@ import { EnvironmentConfig } from '@talisoft/common';
 
 export const environment: EnvironmentConfig = {
   production: false,
-  apiUrl: 'https://localhost:8081',
+  apiUrl: 'https://localhost:44363',
   apiKey: '',
 };
