b4f2a24bb15ad6e03ceacc60f076b85987dfb454
Ambroise <bazieambroise@gmail.com>
2024-12-13 19:36:44 +0000

    update environment variables

--- apps/Lotchen/src/environments/environment.ts
+++ apps/Lotchen/src/environments/environment.ts
@@ -2,6 +2,6 @@ import { EnvironmentConfig } from '@talisoft/common';
 
 export const environment: EnvironmentConfig = {
   production: false,
-  apiUrl: 'https://localhost:44363',
+  apiUrl: 'https://localhost:5165',
   apiKey: '',
 };
--- libs/shared/ui/form-field/src/label.component.ts
+++ libs/shared/ui/form-field/src/label.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
 
+/**
+ * Tas Label
+ * 
+ */
 @Component({
   selector: 'tas-label',
   template: `<ng-content></ng-content>`,
