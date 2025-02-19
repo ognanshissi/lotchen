670a7fc15c9f532e526772675d5b756ae3bffd86
Ambroise <bazieambroise@gmail.com>
2025-02-19 12:55:01 +0000

    Add lead module

---
+++ libs/lotchen-api/leads/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/leads/leads.component';
---
+++ libs/lotchen-api/leads/src/lib/leads/leads.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-leads',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './leads.component.html',
+  styleUrl: './leads.component.css',
+})
+export class LeadsComponent {}
