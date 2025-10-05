fc70d65c9f867493995a5bb50b9fa1d8c1d6f82f
Ambroise <bazieambroise@gmail.com>
2025-10-05 10:16:22 +0200

    feat: update summary-field component

--- libs/shared/ui/summary-field/src/summary-field.ts
+++ libs/shared/ui/summary-field/src/summary-field.ts
@@ -2,16 +2,33 @@ import {
   ChangeDetectionStrategy,
   Component,
   input,
+  OnInit,
+  signal,
   ViewEncapsulation,
 } from '@angular/core';
-import { TasLabel } from '@talisoft/ui/form-field';
+import { TasLabel, FormField } from '@talisoft/ui/form-field';
 import { TasIcon } from '@talisoft/ui/icon';
-
+import { AbstractControlValueAccessor } from '@talisoft/ui/core';
+import { CdkConnectedOverlay } from '@angular/cdk/overlay';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { TasInput } from '@talisoft/ui/input';
+1;
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from '@talisoft/ui/button';
 @Component({
   selector: 'tas-summary-field',
   templateUrl: './summary-field.html',
   standalone: true,
-  imports: [TasLabel, TasIcon],
+  imports: [
+    TasLabel,
+    TasIcon,
+    AbstractControlValueAccessor,
+    CdkConnectedOverlay,
+    OverlayModule,
+    FormField,
+    ReactiveFormsModule,
+    ButtonModule,
+  ],
   styles: [
     `
       .actions {
@@ -22,8 +39,23 @@ import { TasIcon } from '@talisoft/ui/icon';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TasSummaryField {
+export class TasSummaryField implements OnInit {
   public label = input();
   public rawField = input.required<string>();
+  public type = input<'text' | 'number' | 'date'>('text');
+  public placeholder = input<string>('');
+  public value = input<string | number | Date | undefined>(undefined);
   public editable = input<boolean>(false);
+
+  public isOpen = signal(false);
+
+  public control = new FormControl();
+
+  public ngOnInit() {
+    this.control.patchValue(this.value());
+  }
+
+  public handleSubmittion() {
+    console.log(this.control.value);
+  }
 }
